65f8f2cea8a6e8bd19b439461bdbd25dca6f4111
mpotech-segovia <carlosegovia_mktg@mpotech.net>
2022-07-08 09:29:12 +0800

    footer

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,20 @@
     <v-main>
       <router-view />
     </v-main>
+    <Footer></Footer>
   </v-app>
 </template>
 
 <script>
   import Header from '@/layout/Header.vue'
+  import Footer from '@/layout/Footer.vue'
 
   export default {
     name: 'App',
 
     components: {
-      Header
+      Header,
+      Footer
     },
 
     data: () => ({
---
+++ src/layout/Footer.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="footer">
+        <v-container>
+            <div class="footer__cont">
+                <div class="footer-logo">
+                    <img src="@/assets/image/logo_footer.png" alt="">
+                </div>
+                <div class="footer-nav">
+                    <router-link to="#">
+                        <div class="nav-item">HOME</div>
+                    </router-link>
+                    <router-link to="#">
+                        <div class="nav-item">ABOUT</div>
+                    </router-link>
+                    <router-link to="#">
+                        <div class="nav-item">PORTFOLIO</div>
+                    </router-link>
+                    <router-link to="#">
+                        <div class="nav-item">CONTACT</div>
+                    </router-link>
+                </div>
+                <div class="footer-trademark">
+                    <h4>© 2022 Carlo Segovia</h4>
+                </div>
+            </div>
+        </v-container>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Footer'
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import '@/assets/scss/layout/_footer.scss';
+</style>
