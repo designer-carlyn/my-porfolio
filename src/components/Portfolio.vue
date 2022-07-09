3343ff23e9969de15849e1de3bc3a3f3c1f2865d
mpotech-segovia <carlosegovia_mktg@mpotech.net>
2022-07-09 13:53:09 +0800

    update portfolio section and update resume

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -4,37 +4,39 @@
             <div class="portfolio__content">
                 <CoolLightBox :items="items" :index="index" @close="index = null"></CoolLightBox>
                 <h1 class="section-title">Look at my <br> recent projects</h1>
-                <div class="portfolio-slider">
-                    <div class="swiper-container">
-                        <div class="swiper-wrapper">
-							<!-- Slides -->
-							<div class="swiper-slide" v-for="(item, i) in projectItem" :key="i">
-								<div class="slide-container">
-									<div class="slide-image">
-										<img :src="item.image" alt="">
+            </div>
+        </v-container>
+        <div class="container--fluid">
+            <div class="portfolio-slider">
+                <div class="swiper-container">
+                    <div class="swiper-wrapper">
+                        <!-- Slides -->
+                        <div class="swiper-slide" v-for="(item, i) in projectItem" :key="i">
+                            <div class="slide-container">
+                                <div class="slide-image">
+                                    <img :src="item.image" alt="">
 
-                                        <div class="slide-action">
-                                            <router-link :to="item.demoLink" v-if="item.hasDemo">
-                                                <v-btn x-large><v-icon>fas fa-external-link-alt</v-icon></v-btn>
-                                            </router-link>
-                                            <v-btn x-large @click="index = i"><v-icon>fas fa-eye</v-icon></v-btn>
-                                        </div>
-									</div>
-								</div>
-							</div>
-						</div>
-                    </div>
-                    <div class="navigation">
-                        <div class="navigation-button navigation-prev-project">
-                            <v-icon>fas fa-arrow-left</v-icon>
-                        </div>
-                        <div class="navigation-button navigation-next-project">
-                            <v-icon>fas fa-arrow-right</v-icon>
+                                    <div class="slide-action">
+                                        <router-link :to="item.demoLink" v-if="item.hasDemo">
+                                            <v-btn x-large><v-icon>fas fa-external-link-alt</v-icon></v-btn>
+                                        </router-link>
+                                        <v-btn x-large @click="index = i"><v-icon>fas fa-eye</v-icon></v-btn>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
+                <div class="navigation">
+                    <div class="navigation-button navigation-prev-project">
+                        <v-icon>fas fa-arrow-left</v-icon>
+                    </div>
+                    <div class="navigation-button navigation-next-project">
+                        <v-icon>fas fa-arrow-right</v-icon>
+                    </div>
+                </div>
             </div>
-        </v-container>
+        </div>
     </div>
 </template>
 
@@ -121,18 +123,20 @@
         mounted() {
             new Swiper('.swiper-container', {
 				modules: [Navigation],
-                slidesPerView: 2,
-                spaceBetween: 30,
-                autoplay: {
-                    delay: 5000,
-                },
+                slidesPerView: 3,
+                spaceBetween: 0,
+                centeredSlides: true,
+                loop: true,
+                // autoplay: {
+                //     delay: 5000,
+                // },
 				navigation: {
 					nextEl: '.navigation-next-project',
 					prevEl: '.navigation-prev-project',
 				},
                 breakpoints: {
                         1280: {
-                            slidesPerView: 2,
+                            slidesPerView: 3,
                         },
                         320: {
                             slidesPerView: 1,
